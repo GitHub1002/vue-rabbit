427831ac4fd91e5d04c6c3c1a05264247deca385
fengmk <2508812594@qq.com>
2024-10-20 21:18:58 +0800

    layout静态结构搭建

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -1,11 +1,16 @@
 <script setup lang="ts" name="Login">
-
+    import LayoutHeader from './components/LayoutHeader.vue';
+    import LayoutFooter from './components/LayoutFooter.vue';
+    import LayoutNav from './components/LayoutNav.vue';
 </script>
 
 <template>
     <div>
+        <LayoutHeader></LayoutHeader>
+        <LayoutNav></LayoutNav>
         我是Layout组件
         <router-view></router-view>
+        <LayoutFooter></LayoutFooter>
     </div>
 </template>
 
