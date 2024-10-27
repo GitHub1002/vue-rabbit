9c26024e424edd881a84c2ee93c057eda9b2e228
fengmk <2508812594@qq.com>
2024-10-27 17:28:00 +0800

    导航栏吸顶

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -2,10 +2,12 @@
     import LayoutHeader from './components/LayoutHeader.vue';
     import LayoutFooter from './components/LayoutFooter.vue';
     import LayoutNav from './components/LayoutNav.vue';
+    import LayoutFixed from './components/LayoutFixed.vue';
 </script>
 
 <template>
     <div>
+        <LayoutFixed></LayoutFixed>
         <LayoutNav></LayoutNav>
         <LayoutHeader></LayoutHeader>
         我是Layout组件
