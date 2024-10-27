aa293f469e955914ca5c8e8db9481967701101ea
fengmk <2508812594@qq.com>
2024-10-27 14:08:24 +0800

    静态结构优化

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -1,4 +1,4 @@
-<script setup lang="ts" name="Login">
+<script setup lang="ts" name="Index">
     import LayoutHeader from './components/LayoutHeader.vue';
     import LayoutFooter from './components/LayoutFooter.vue';
     import LayoutNav from './components/LayoutNav.vue';
@@ -6,8 +6,8 @@
 
 <template>
     <div>
-        <LayoutHeader></LayoutHeader>
         <LayoutNav></LayoutNav>
+        <LayoutHeader></LayoutHeader>
         我是Layout组件
         <router-view></router-view>
         <LayoutFooter></LayoutFooter>
