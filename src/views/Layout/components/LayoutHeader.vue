20f492121b24e8afe1e76cf2a9eef43365041a40
fengmk <2508812594@qq.com>
2024-10-27 16:31:19 +0800

    layout-一级导航渲染

--- src/views/Layout/components/LayoutHeader.vue
+++ src/views/Layout/components/LayoutHeader.vue
@@ -1,5 +1,18 @@
 <script setup lang="ts">
+    import { onMounted, ref } from 'vue'
+    import { getCategoryAPI } from '@/apis/layout'
 
+    const categoryList = ref([])
+    
+    const getCategory = async () => {
+        const res = await getCategoryAPI()
+        console.log(res.data.result)
+        categoryList.value = res.data.result
+    }
+
+    onMounted(() => {
+        getCategory()
+    })
 </script>
 
 <template>
@@ -9,17 +22,8 @@
                 <RouterLink to="/">小兔鲜</RouterLink>
             </h1>
             <ul class="app-header-nav">
-                <li class="home">
-                    <RouterLink to="/">首页</RouterLink>
-                </li>
-                <li>
-                    <RouterLink to="/">居家</RouterLink>
-                </li>
-                <li>
-                    <RouterLink to="/">美食</RouterLink>
-                </li>
-                <li>
-                    <RouterLink to="/">服饰</RouterLink>
+                <li class="home" v-for="item in categoryList" :key="item.id">
+                    <RouterLink to="/">{{item.name}}</RouterLink>
                 </li>
             </ul>
             <div class="search">
