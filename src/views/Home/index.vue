da5441972120225d99be8dbaadb6f68bb37d5ccd
fengmk <2508812594@qq.com>
2024-11-01 00:20:20 +0800

    HomePanel组件封装

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -4,7 +4,7 @@
     import HomeNew from './components/HomeNew.vue'
     import HomeHot from './components/HomeHot.vue'
     import HomeProduct from './components/HomeProduct.vue'
-    // import HomePanel from './components/HomePanel.vue'
+    import HomePanel from './components/HomePanel.vue'
 </script>
 
 <template>
@@ -15,4 +15,11 @@
     <HomeNew />
     <HomeHot />
     <HomeProduct />
+    <!-- <HomePanel /> -->
+    <!-- <HomePanel title="热门推荐" sub-title="精选好物">
+        <div>haowu</div>
+    </HomePanel>
+    <HomePanel title="热门推荐" sub-title="精选好物">
+        <div>haowu</div>
+    </HomePanel> -->
 </template>
