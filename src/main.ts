2466b287b8d4d2d27fd8af69912063622af1835d
fengmk <2508812594@qq.com>
2024-11-03 17:46:18 +0800

    图片懒加载

--- src/main.ts
+++ src/main.ts
@@ -8,10 +8,14 @@ import router from './router'
 
 // 引入初始化样式文件
 import '@/styles/common.scss'
+// 引入图片延迟加载
+import { lazyPlugin } from '@/directives/index'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+// 使用图片延迟加载插件
+app.use(lazyPlugin)
 
 app.mount('#app')
