75a916e74ff340e4142a03e2d1f31dd57429e247
fengmk <2508812594@qq.com>
2024-11-17 23:46:20 +0800

    图片预览功能-小图切换大图

--- src/views/Detail/index.vue
+++ src/views/Detail/index.vue
@@ -3,6 +3,7 @@
     import { useRoute } from 'vue-router'
     import { getDetailAPI } from '@/apis/detail'
     import  DetailHot  from '@/views/Detail/components/DetailHot.vue'
+    import ImageView from '@/components/ImageView/index.vue'
 
     // 获取详情页参数
     const route = useRoute()
@@ -39,7 +40,7 @@
                     <div class="goods-info">
                         <div class="media">
                             <!-- 图片预览区 -->
-                            
+                            <ImageView />
                             <!-- 统计数量 -->
                             <ul class="goods-sales">
                                 <li>
